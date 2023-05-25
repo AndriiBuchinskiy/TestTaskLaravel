cf050d1818e5f73c366669072ce5987756c9d57d
Jonny254 <buchinskia@gmail.com>
2023-05-25 11:43:10 +0300

    first version33

--- resources/views/admin/post/create.blade.php
+++ resources/views/admin/post/create.blade.php
@@ -8,7 +8,7 @@
 
         <div class="container mt-4">
 
-            <form method="POST" action="{{ route('posts.store') }}" enctype="multipart/form-data">
+            <form method="POST" action="{{ route('posts.store') }}">
                 @csrf
                 <div class="mb-3">
                     <label for="name" class="form-label">Назва</label>
@@ -16,39 +16,42 @@
                            type="text"
                            class="form-control shadow-none @error('title') is-invalid @enderror"
                            name="title"
-                           placeholder="Title" required>
+                           placeholder="Title" >
+                    @error('title')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
                 </div>
 
                 <input type="hidden" name="user_id" value="{{ auth()->id() }}">
-                @error('title')
-                <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
 
                 <div class="mb-3">
                     <label for="description" class="form-label">Контент</label>
                     <textarea type="text"
                               class="form-control shadow-none @error('content') is-invalid @enderror"
                               name="content"
-                              placeholder="Content" required>
-                         {{ old('content') }}
-                    </textarea>
+                              placeholder="Content" >
+                     {{ old('content') }}
+                </textarea>
+                    @error('content')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
                 </div>
-
-                @error('content')
-                <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
 
                 <div class="mb-3">
                     <label for="category_id" class="form-label">Виберіть категорію</label>
                     <select class="form-select shadow-none @error('category_id') is-invalid @enderror" name="category_id"
                             id="inputGroupSelect01">
-                        <option selected>Виберіть категорію</option>
+                        <option value="" selected disabled>Виберіть категорію</option>
                         @foreach($categories as $category)
                             <option
                                     value="{{ $category->id }}" {{ old('category_id') == $category->id ? 'selected' : ''}}>{{ $category->name }}</option>
                         @endforeach
                     </select>
+                    @error('category_id')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
                 </div>
+
                 <div class="mb-3">
                     <label for="tags" class="form-label">Виберіть теги</label>
                     <select class="form-select shadow-none @error('tags') is-invalid @enderror" name="tags[]"
@@ -58,26 +61,21 @@
                                     value="{{ $tag->id }}" {{ in_array($tag->id, old('tags') ?: []) ? 'selected' : ''}}>{{ $tag->name }}</option>
                         @endforeach
                     </select>
+                    @error('tags')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
                 </div>
 
-                <label for="img_path" class="form-label">Зображення</label>
-                <div class="mb-3 input-group">
+                <div class="mb-3">
+                    <label for="img_path" class="form-label">Зображення</label>
                     <input type="file"
                            class="form-control shadow-none @error('img_path') is-invalid @enderror"
                            name="img_path"
-                           placeholder="Image" required>
+                           placeholder="Image" >
+                    @error('img_path')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
                 </div>
-
-                @error('img_path')
-                <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-
-                @error('category_id')
-                <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-                @error('$tags')
-                <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
 
                 <button type="submit" class="btn btn-outline-primary shadow-none">Зберегти пост</button>
                 <a href="{{ route('posts.index') }}" class="btn btn-outline-secondary shadow-none">Назад</a>
